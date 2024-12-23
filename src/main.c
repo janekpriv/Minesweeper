0c9807ab3832a51312b766d266a590426717a8b1
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2024-12-23 19:24:25 +0100

    revealing fields functionality

--- src/main.c
+++ src/main.c
@@ -4,15 +4,21 @@
 int main() {
     Board board;
     board.difficulty = 1;
-    board.rows = 5;
-    board.cols = 5;
-    board.num_mines = 5;
+    board.rows = 10;
+    board.cols = 10;
+    board.num_mines = 10;
     board.placed_flags = 0;
 
     // Generate the board with a safe starting point at (0, 0)
     set_up_board(&board, 0, 0);
 
     // Print the generated board
+    print_board(&board);
+    printf("---------------------\n");
+    print_minefield(&board);
+
+    reveal_field(&board, 0, 0);
+
     print_board(&board);
     printf("---------------------\n");
     print_minefield(&board);
