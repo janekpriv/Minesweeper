02ea31075129553a00d0f8b0e0cad8d5563d6392
Janek <janekpriv@gmail.com>
2025-01-11 19:11:03 +0100

    added getopt and file handling

--- src/main.c
+++ src/main.c
@@ -8,8 +8,37 @@
 
 int main(int argc, char** argv)
 {
+    int options;
+    int f_flag =0;
+    int r_flag; // rows
+    int c_flag; // columns
+    char* file_path = NULL;
+
+    while((options = getopt(argc, argv, "f")) =-1 ){
+        switch (options)
+        {
+        case 'f':
+            f_flag = 1;
+            file_path = optarg;
+            break;
+
+        case '?':
+
+            if(optopt == 'f'){
+                fprintf(stderr, "Opcja -f wymaga podania sciezki do pliku\n");
+            }else{
+                fprintf(stderr, "Nieznana opcja -%c\n", optopt);
+            }
+
+            return 1;
+        }
+    }
+
+    if(file_path){
+        test(file_path);
+    }else{
     game_start();
     display_scoreboard();
-
+    }
     return 0;
 }
